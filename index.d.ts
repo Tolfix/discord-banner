1feb818b3a50960931ad404e6527e5734fbe88b4
Tolfix <matteus12122@gmail.com>
2021-07-21 14:05:40 +0200

    Updated types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,27 @@
+import { Client } from "discord.js";
+
 declare module "discord.js" {
-    export class ClientUser {
-        getUserBanner(clientId?: string): Promise<String|Null>
+    export interface ClientUser {
+        /**
+         * 
+         * @param {String} clientId 
+         * @returns Promise<String|null>
+         * @description Gives the banner from the client id.
+         */
+        getUserBanner(clientId?: string): Promise<String|null>
     }
+
+    export interface User {
+        /**
+         * 
+         * @param {String} clientId 
+         * @returns Promise<String|null>
+         * @description Gives the banner from the client id.
+         */
+        getUserBanner(clientId?: string): Promise<String|null>
+    }
+}
+
+declare module "discord-banner" {
+    export default function (client: Client): void
 }

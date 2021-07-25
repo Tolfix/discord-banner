4dba900f252817c848ae1278dd4bf045641c77f8
Tolfx <matteus12122@gmail.com>
2021-07-25 11:34:28 +0200

    More tests

--- test/index.test.js
+++ test/index.test.js
@@ -12,6 +12,24 @@ client.on("message", async (message) => {
     console.log(await client.user.bannerURL("269870630738853888"))
 });
 
-GetUserBanner("269870630738853888").then(banner => console.log(banner));
+console.time("first_time");
+GetUserBanner("269870630738853888").then(banner => {
+    console.log(banner)
+    console.timeEnd("first_time")
+
+    console.time("cache")
+    GetUserBanner("269870630738853888").then(banner_two => {
+        console.log(banner_two)
+        console.timeEnd("cache")
+    });
+
+    setTimeout(() => {
+        console.time("recache")
+        GetUserBanner("269870630738853888").then(banner_three => {
+            console.log(banner_three)
+            console.timeEnd("recache")
+        });
+    }, 1.2*60*1000)
+});
 
 client.login(process.env.TOKEN)

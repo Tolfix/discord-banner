ad90340fcc9c897495c9e022e68864b5c3927333
Matteus <tolfx@tolfix.com>
2021-07-31 21:19:15 +0200

    Small changes

--- test/index.test.js
+++ test/index.test.js
@@ -3,33 +3,35 @@ require("dotenv").config();
 require("../lib/index")(process.env.TOKEN);
 const client = new (require("discord.js").Client);
 // initializes the package.
-const { ExtendedUser, GetUserBanner } = require("../lib/index")
+const { ExtendedUser, getUserBanner } = require("../lib/index")
 
 client.on("message", async (message) => {
-    console.log(message.author instanceof ExtendedUser);
-    console.log(await message.author.bannerURL());
-    // Possible to get it from the ClientUser.
-    console.log(await client.user.bannerURL("269870630738853888"))
+    if(message.author.bot) return;
+    message.channel.send(await message.author.bannerURL());
 });
 
 console.time("first_time");
-GetUserBanner("269870630738853888").then(banner => {
+getUserBanner("269870630738853888").then(banner => {
     console.log(banner)
     console.timeEnd("first_time")
 
     console.time("cache")
-    GetUserBanner("269870630738853888").then(banner_two => {
+    getUserBanner("269870630738853888").then(banner_two => {
         console.log(banner_two)
         console.timeEnd("cache")
     });
 
     setTimeout(() => {
         console.time("recache")
-        GetUserBanner("269870630738853888").then(banner_three => {
+        getUserBanner("269870630738853888").then(banner_three => {
             console.log(banner_three)
             console.timeEnd("recache")
         });
     }, 1.2*60*1000)
 });
+
+getUserBanner("754482040178737343", {
+    token: process.env.TOKEN
+}).then(console.log)
 
 client.login(process.env.TOKEN)
